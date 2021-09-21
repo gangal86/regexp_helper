aaa516240e0ab2c11fc18dc3fc3874eefb5dc030
Roman Gangal <gangal33@gmail.com>
2021-09-21 14:23:58 +0300

    cheat sheet anchors category component completed

--- src/components/cheat-sheet-categories/Anchors.vue
+++ src/components/cheat-sheet-categories/Anchors.vue
@@ -1,12 +1,85 @@
 <template>
   <div class="q-pa-md">
-     Якоря
+     <q-table
+      title="Якоря"
+      :data="data"
+      :columns="columns"
+      :pagination="{rowsNumber:10}"
+      bordered
+      flat
+      row-key="name"
+      hide-header
+      hide-bottom
+    >
+      <template v-slot:body="props">
+        <q-tr :class="{'bg-teal-1': props.pageIndex % 2 == 0}" :props="props">
+          <q-td key="description" :props="props">
+            <q-badge class="text-subtitle1" color="primary">
+              {{ props.row.description }}
+            </q-badge>
+          </q-td>
+          <q-td key="pattern" :props="props">
+            <div class="text-subtitle1">
+              {{ props.row.pattern}}
+            </div>
+          </q-td>
+        </q-tr>
+      </template>
+    </q-table>
   </div>
 </template>
 
 <script>
   export default {
-    name: "Anchors"
+    name: "Anchors",
+    data () {
+    return {
+      columns: [
+        {
+          name: 'pattern',
+          align: 'center',
+        },
+        { name: 'description', 
+          align: 'left', 
+        },
+      ],
+
+      data: [
+        {
+          pattern: 'Начало строки',
+          description: "^"
+        },
+        {
+          pattern: 'Начало текста',
+          description: "\\A"
+        },
+        {
+          pattern: 'Конец строки',
+          description: "$"
+        },
+        {
+          pattern: 'Конец текста',
+          description: "\\Z"
+        },
+        {
+          pattern: 'Граница слова',
+          description: "\\b"
+        },
+        {
+          pattern: 'НЕ граница слова',
+          description: "\\B"
+        },
+        {
+          pattern: 'Начало слова',
+          description: "\\<"
+        },
+        {
+          pattern: 'Конец слова',
+          description: "\\>"
+        }
+      ]
+    }
+  }
     
   }
 </script>
