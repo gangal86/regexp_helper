5834e63d4ec8198f4ae442351bb3614054f1c221
Roman Gangal <gangal33@gmail.com>
2021-10-05 12:22:53 +0300

    about program dialog completed

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -23,9 +23,42 @@
         >
           <country-flag country="rus" />
         </q-btn>
-        <q-btn flat round dense icon="more_vert" />
+        <q-btn flat round dense icon="more_vert">
+          <q-menu auto-close>
+            <q-list class="menu-options">
+              <q-item @click="aboutProgram = !aboutProgram" clickable>
+                <q-item-section class="text-caption text-weight-bold text-primary">{{
+                  $t("labelMenuAbout")
+                }}</q-item-section>
+              </q-item>
+            </q-list>
+          </q-menu>
+        </q-btn>
       </q-toolbar>
     </q-header>
+    <q-dialog v-model="aboutProgram">
+      <q-card>
+        <q-card-section class="column q-pb-xs">
+          <q-card>
+            <q-card-section class="bg-primary text-white">
+              <div class="text-h6">RegExp Helper</div>
+              <div class="text-subtitle2">
+                {{ $t('aboutProgramDescribe') }}
+              </div>
+            </q-card-section>
+            <q-card-actions vertical align="left">
+              <div class="text-subtitle2"><span class="text-weight-regular">{{ $t('aboutProgramDeveloper') }} </span><span class="text-weight-bold">Roman Gangal</span></div>
+              <div class="text-subtitle2">
+                <span class="text-weight-regular">{{ $t('aboutProgramWebsite') }} </span><a href="https://gangal.pro" target="_blank" class="about-program-link"><span class="text-weight-bold">Gangal.Pro</span></a>
+              </div>
+            </q-card-actions>
+          </q-card>
+        </q-card-section>
+        <q-card-actions align="right">
+          <q-btn flat label="OK" color="primary" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -39,6 +72,7 @@ export default {
   name: "MainLayout",
   data() {
     return {
+      aboutProgram: false,
       lang: this.$i18n.locale,
       countryFlagStatus: LocalStorage.getItem("countryFlagStatus") || false
     };
@@ -70,5 +104,14 @@ export default {
 .main-layout {
   max-width: 1000px;
   margin: 0px auto;
+}
+.menu-options {
+  min-width: 120px;
+}
+.about-program-link {
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
+  }
 }
 </style>
