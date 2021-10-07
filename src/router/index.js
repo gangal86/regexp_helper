d04718a9e48e8165ac7bc7bd9e82ab96efd0806d
Roman Gangal <gangal33@gmail.com>
2021-10-07 10:59:38 +0300

    fix mode history

--- src/router/index.js
+++ src/router/index.js
@@ -17,8 +17,6 @@ Vue.use(VueRouter)
 export default function (/* { store, ssrContext } */) {
   const Router = new VueRouter({
     scrollBehavior: () => ({ x: 0, y: 0 }),
-    mode: 'history',
-    hash: false,
     routes,
 
     // Leave these as they are and change in quasar.conf.js instead!
